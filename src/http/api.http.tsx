334b8d5f32ae49d0b6994ba3d2e2190f3b9e18a8
Raj <rajksd9@gmail.com>
2024-06-16 23:44:55 +0530

    updaetd url

--- src/http/api.http.tsx
+++ src/http/api.http.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4001",
+  baseURL: "https://dashboardbackend-g2gj.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
